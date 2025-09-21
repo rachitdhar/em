dbaac9c7633da8ba8a6ae05f0c7973599925c947
rachitdhar <75770595+rachitdhar@users.noreply.github.com>
2025-09-21 11:44:16 +0530

    added blank type checker file

---
+++ type_checker.cpp
@@ -0,0 +1,22 @@
+//
+// type_checker.cpp
+//
+
+
+#include "parser.h"
+
+
+void perform_type_checking(std::vector<AST_Expression*> *ast)
+{
+
+}
+
+
+int main()
+{
+    Lexer *lexer = perform_lexical_analysis("program.txt");
+    auto *ast = parse_tokens(lexer);
+
+    perform_type_checking(ast);
+    return 0;
+}

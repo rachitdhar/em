daaf0d9715e68bcf77e94c09b60899c9603fa7b8
Rachit Dhar <75770595+rachitdhar@users.noreply.github.com>
2026-02-20 10:03:27 +0530

    Update build-msvc.bat

--- build-msvc.bat
+++ build-msvc.bat
@@ -1,10 +1,14 @@
 @echo off
 
-if "%1"=="-debug" (
+set DEBUG_FLAG=/W0
+set DEBUG_LINK=
+
+if "%1"=="Debug" (
    set DEBUG_FLAG=/Od
-) else (
-   set DEBUG_FLAG=/W0
+   set DEBUG_LINK=/DEBUG
 )
+
+echo Build type: %1
 
 cl ^
 %DEBUG_FLAG% ^
@@ -21,4 +25,5 @@ lexer.cpp parser.cpp ir_generator.cpp main.cpp ^
 ntdll.lib ^
 /LIBPATH:"D:\softwares\clang+llvm-21.1.8-x86_64-pc-windows-msvc\lib" ^
 D:\softwares\clang+llvm-21.1.8-x86_64-pc-windows-msvc\lib\*.lib ^
+%DEBUG_LINK% ^
 /OUT:emc.exe
